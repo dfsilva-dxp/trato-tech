cae7d64f614696a279ba6e2ff7377523821b7043
Daniel Silva <dfsilva.dxp@gmail.com>
2023-06-14 18:12:41 -0300

    chore: este commit deleta os templates de style e test que não usarei no projeto.

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -19,16 +19,6 @@ module.exports = (plop) => {
         type: "add",
         path: "../src/components/{{pascalCase name}}/styles.ts",
         templateFile: "template/styles.ts.hbs"
-      },
-      {
-        type: "add",
-        path: "../src/stories/components/{{camelCase name}}.stories.tsx",
-        templateFile: "template/stories.tsx.hbs"
-      },
-      {
-        type: "add",
-        path: "../src/tests/components/{{camelCase name}}.test.tsx",
-        templateFile: "template/test.tsx.hbs"
       }
     ]
   });
